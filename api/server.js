366e60b44fc430a26decc277be11f0035235ff76
siratl <elishubax@gmail.com>
2019-02-28 20:07:42 -0800

    Made code DRY

--- api/server.js
+++ api/server.js
@@ -1,42 +1,14 @@
 const express = require('express');
 
-const knex = require('knex');
-const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets.js');
-
-const knexConfig = require('../knexfile.js');
-const db = knex(knexConfig.development);
 const configureMiddleware = require('./middleware');
 const authRouter = require('../auth/authRouter');
+const usersRouter = require('../users/usersRoutes');
 
 const server = express();
 
 configureMiddleware(server);
+
 server.use('/api/auth', authRouter);
-
-//********** RESTRICTING MIDDLEWARE ************/
-function restricted(req, res, next) {
-  const token = req.headers.authorization;
-  if (token) {
-    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-      if (err) {
-        res.status(401).json({ message: 'Invalid token' });
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.status(401).json({ message: 'No token provided' });
-  }
-}
-
-//******************** GET ALL USERS ******************/
-server.get('/api/users', restricted, (req, res) => {
-  db('users')
-    .then(users => {
-      res.status(200).json({ users });
-    })
-    .catch();
-});
+server.use('/api/users', usersRouter);
 
 module.exports = server;
--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -1,87 +1,41 @@
 const express = require('express');
-
 const knex = require('knex');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
 const knexConfig = require('../knexfile.js');
-
 const db = knex(knexConfig.development);
-
-const secret = 'keep this secret safe';
-
-//************ GENERATE TOKEN ***************/
-function generateToken(user) {
-  const payload = {
-    username: user.username,
-  };
-
-  const options = {
-    expiresIn: '1h',
-    jwtid: '54321',
-  };
-  return jwt.sign(payload, secret, options);
-}
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 //********** RESTRICTING MIDDLEWARE ************/
 function restricted(req, res, next) {
   const token = req.headers.authorization;
-
   if (token) {
-    jwt.verify(token, secret, (err, decodedToken) => {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
       if (err) {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: 'Invalid token' });
       } else {
         next();
       }
     });
   } else {
-    res.status(401).json({ message: 'No Credentials provided' });
+    res.status(401).json({ message: 'No token provided' });
   }
 }
 
-//******************** REGISTER NEW USER ******************/
-router.post('/register', (req, res) => {
-  const user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10);
-  user.password = hash;
-
-  db('users')
-    .insert(user)
-    .then(ids => {
-      const id = ids[0];
-
-      const token = generateToken(user);
-      res.status(201).json({ id, token });
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
-});
-
-//******************** LOGIN USER ******************/
-router.post('/login', (req, res) => {
-  const { username, department, password } = req.body;
-  const creds = { username, department, password };
-
-  db('users')
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        const token = makeToken(user);
-
-        res.status(200).json({ token });
-      } else {
-        res.status(401).json({ message: 'You shall not pass!' });
-      }
-    })
-    .catch(err => res.status(500).send(err));
-});
+//******************** CHECKROLE MIDDLEWARE ******************/
+// function checkRole(role) {
+//   return function(req, res, next) {
+//     if (req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
+//       next();
+//     } else {
+//       res.status(403).json({ you: 'you have no power here!' });
+//     }
+//   };
+// }
 
 //******************** GET ALL USERS ******************/
-router.get('/users', restricted, (req, res) => {
+router.get('/', restricted, (req, res) => {
   db('users')
     .select('id', 'username', 'password', 'department')
     .then(users => {
